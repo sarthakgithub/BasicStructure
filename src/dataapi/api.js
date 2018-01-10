760c0020f9f97707d4349e746ce46d4885821ff3
sakh20 <32352591+sakh20@users.noreply.github.com>
2018-01-10 17:53:36 +0530

    Update api.js

--- src/dataapi/api.js
+++ src/dataapi/api.js
@@ -13,3 +13,37 @@ export const postLoginInfo = (updatedObject) => {
         return result;
     })
 }
+
+export const fetching = () => {
+    return new Promise(function(resolve,reject){
+        var req = new XMLHttpRequest();
+        req.open('GET','http://localhost:3000/items');
+
+        req.onload = function(){
+            if(req.status == 200){
+                resolve(req.response);
+            }else{
+                reject(Error(req.statusText))
+            }
+        };
+
+        //handling network errors
+        req.onerror = function(){
+            reject(Error('Network Error'));
+        };
+
+        //Make request
+        req.send();
+    })
+}
+
+export function fetchingItems() {
+    return fetching().then((response) => {
+        //console.log('Items response',(JSON.parse(response)).shows);
+        return ((JSON.parse(response)).shows);
+    },(error) => {
+        console.log('Failed !',error);
+    }).then((result) => {
+        return result;
+    })
+}
